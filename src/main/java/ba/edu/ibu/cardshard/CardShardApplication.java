114717995f5f28d7f20c54f77e05c385c510f72f
SamirSadikovic <samir.sadikovic.bih@gmail.com>
2023-11-10 12:52:51 +0100

    Initial commit

---
+++ src/main/java/ba/edu/ibu/cardshard/CardShardApplication.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.cardshard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardShardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardShardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ba/edu/ibu/cardshard/CardShardApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.cardshard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardShardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
