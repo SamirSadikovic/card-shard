f7bae550638ac0a7951050de1c152e22c8160095
SamirSadikovic <samir.sadikovic.bih@gmail.com>
2024-01-21 22:53:33 +0100

    updated default preview

--- src/main/java/ba/edu/ibu/cardshard/view/card-shard/src/constants.ts
+++ src/main/java/ba/edu/ibu/cardshard/view/card-shard/src/constants.ts
@@ -1,7 +1,7 @@
 export const BASE_URL = "https://card-shard-server.onrender.com/api";
 
 export const defaultPreview = {
-  imageLink: "../src/assets/img/card_images/large/83764719.jpg",
+  imageLink: "https://i.imgur.com/P1a0GQe.jpg",
   name: "Monster Reborn",
   desc: "Target 1 monster in either GY; Special Summon it."
 }
