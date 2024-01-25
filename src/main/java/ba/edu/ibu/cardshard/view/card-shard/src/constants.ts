7872c654842817e8c5c228929f4daf762779d865
SamirSadikovic <samir.sadikovic.bih@gmail.com>
2024-01-25 17:43:00 +0100

    server link update

--- src/main/java/ba/edu/ibu/cardshard/view/card-shard/src/constants.ts
+++ src/main/java/ba/edu/ibu/cardshard/view/card-shard/src/constants.ts
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://card-shard-server.onrender.com/api";
-export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = "https://card-shard-server-sdp.onrender.com/api";
+// export const BASE_URL = "http://localhost:8080/api";
 
 export const defaultPreview = {
   imageLink: "https://i.imgur.com/P1a0GQe.jpg",
